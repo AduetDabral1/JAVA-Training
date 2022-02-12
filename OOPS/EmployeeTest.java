48e0fca973776c7c25c14cb19226f00a1bd11a78
Aduet Dabral <98040707+AduetDabral1@users.noreply.github.com>
2022-02-12 18:42:45 +0530

    Add files via upload

---
+++ OOPS/EmployeeTest.java
@@ -0,0 +1,9 @@
+public class EmployeeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EmployeeSalarySlip obj = new EmployeeSalarySlip(101, "aDuET daBRAl", 50000);
+		System.out.println(obj.showEmp());
+	}
+
+}
